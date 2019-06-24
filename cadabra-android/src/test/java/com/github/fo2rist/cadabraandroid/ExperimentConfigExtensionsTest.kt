d2e87bd0d2ddbb7ad1217f688e1e7971b2b67c0b
fo2rist <fo2@inbox.ru>
2019-06-23 22:07:53 -0700

    Add Json parsing support for Cadabra Android Config can be parsed directly from json string

---
+++ cadabra-android/src/test/java/com/github/fo2rist/cadabraandroid/ExperimentConfigExtensionsTest.kt
@@ -0,0 +1,41 @@
+package com.github.fo2rist.cadabraandroid
+
+import com.github.fo2rist.cadabraandroid.testdata.EMPTY_EXPERIMENT_CONFIG
+import com.github.fo2rist.cadabraandroid.testdata.INCORRECT_EXPERIMENT_CONFIG
+import com.github.fo2rist.cadabraandroid.testdata.SINGLE_EXPERIMENT_CONFIG
+import com.github.fo2rist.cadabraandroid.testdata.TWO_EXPERIMENTS_CONFIG
+import org.json.JSONException
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class ExperimentConfigExtensionsTest {
+
+    @Test(expected = JSONException::class)
+    fun `configFromJson throws exception for incorrect Json`() {
+        configFromJson(INCORRECT_EXPERIMENT_CONFIG)
+    }
+
+    @Test
+    fun `configFromJson is empty for empty Json`() {
+        val experimentsConfig = configFromJson(EMPTY_EXPERIMENT_CONFIG)
+
+        assertEquals(0, experimentsConfig.entries.size)
+    }
+
+    @Test
+    fun `configFromJson can parse single entry`() {
+        val experimentsConfig = configFromJson(SINGLE_EXPERIMENT_CONFIG)
+
+        assertEquals(1, experimentsConfig.entries.size)
+    }
+
+    @Test
+    fun `configFromJson can parse multiple entiries`() {
+        val experimentsConfig = configFromJson(TWO_EXPERIMENTS_CONFIG)
+
+        assertEquals(2, experimentsConfig.entries.size)
+    }
+}
